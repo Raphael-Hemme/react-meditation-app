7a45f41e4851c93c9b89e634cee21ab2726378b7
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-05-28 19:18:41 +0200

    misc: Add root.tsx in new routes directory to hold the layout componen soon - maybe rename accordingly

---
+++ src/routes/root.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const root = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li><a href="/">Main</a></li>
+          <li><a href="/practice">practice</a></li>
+          <li><a href="/stats">Stats</a></li>
+        </ul>
+      </nav>
+      <main>
+        <h1>Present</h1>
+        <p>Present is a simple app that helps you practice be present in the moment.</p>
+      </main>
+    </>
+  )
+}
