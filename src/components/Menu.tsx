5ad6e358733af83587f5274c5349f93d1a8909bd
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-06-21 12:52:20 +0200

    cosmetic: Add a transition to the menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,27 +1,43 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
-export const Menu = (
-  {toggleMenu, menuIsOpen}: {toggleMenu: () => void, menuIsOpen: boolean}
-) => {
+export const Menu = ({
+  toggleMenu,
+  menuIsOpen,
+}: {
+  toggleMenu: () => void;
+  menuIsOpen: boolean;
+}) => {
   return (
     <div>
-
-      <button onClick={toggleMenu} className="fixed top-5 left-5 p-2 z-10 bg-gradient-to-b from-green-500 to-blue-500 text-white rounded-full">
+      <button
+        onClick={toggleMenu}
+        className="fixed left-7 top-7 z-10 rounded-full bg-gradient-to-b from-green-500 to-blue-500 p-2 text-white"
+      >
         <img src="/src/assets/menu_icon.svg" />
       </button>
 
-      <div className={`${menuIsOpen ? 'w-100' : 'w-0'} fixed top-0 right-0 bottom-0 left-0 bg-indigo-900 bg-opacity-80 backdrop-blur`}></div>
+      <div
+        className={`${menuIsOpen ? 'w-full' : 'w-0'} fixed bottom-0 left-0 right-0 top-0 bg-indigo-900 bg-opacity-80 backdrop-blur transition-all duration-300 ease-in-out`}
+      ></div>
 
-      <nav className={`${menuIsOpen ? 'w-1/2 px-6 border' : 'w-0 px-0 border-0'} fixed top-0 left-0 bottom-0 py-28 bg-indigo-900 bg-opacity-40 overflow-hidden border-white border-opacity-20`}>
+      <nav
+        className={`${menuIsOpen ? 'w-1/2 border px-6' : 'w-0 border-0 px-0'} fixed bottom-0 left-0 top-0 overflow-hidden border-white border-opacity-20 bg-indigo-900 bg-opacity-40 py-28 transition-all duration-200 ease-in-out`}
+      >
         <ul className="flex flex-col justify-between gap-8 font-bold text-gray-300">
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/practice">Practice</NavLink></li>
-          <li><NavLink to="/stats">Stats</NavLink></li>
-          <li><NavLink to="/settings">Settings</NavLink></li>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/practice">Practice</NavLink>
+          </li>
+          <li>
+            <NavLink to="/stats">Stats</NavLink>
+          </li>
+          <li>
+            <NavLink to="/settings">Settings</NavLink>
+          </li>
         </ul>
       </nav>
-
-      
     </div>
-  )
-}
+  );
+};
