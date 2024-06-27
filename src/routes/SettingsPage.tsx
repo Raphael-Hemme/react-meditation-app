203ad10249ba8635587f2e82b5d74e62edc324ed
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-06-27 17:37:10 +0200

    core: Work on useReducer and start work on SettingsPage

--- src/routes/SettingsPage.tsx
+++ src/routes/SettingsPage.tsx
@@ -1,9 +1,36 @@
+import { useState } from 'react';
 import { PageTitle } from '../components/PageTitle';
 
 export const SettingsPage = () => {
+  // Time in full minutes since sub-minute adjustment should not be necessary-> default is 5 minutes
+  const [minuteDuration, setMinuteDuration] = useState(5);
+
+  const handleDurationChange = (val: number) => {
+    setMinuteDuration(val);
+    // Todo: dispatch action to set app state
+  };
+
   return (
     <div className="h-full w-full">
       <PageTitle pageTitle="Settings" />
+      <section className="flex flex-col">
+        <div className="flex justify-center gap-5">
+          <label htmlFor="durationRange">Duration</label>
+
+          <input
+            type="range"
+            id="durationRange"
+            name="durationRange"
+            min="1"
+            max="600"
+            value={minuteDuration}
+            onChange={(e) => handleDurationChange(+e.target.value)}
+            className="w-2/3 accent-violet-500"
+          />
+          <p>{minuteDuration} minutes</p>
+        </div>
+        <p>How long do you want to meditate?</p>
+      </section>
     </div>
   );
 };
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,17 +2,40 @@ import { Outlet } from 'react-router-dom';
 import { Menu } from '../components/Menu';
 import { useReducer } from 'react';
 
-const reducer = (state, action) => {};
+type AppState = {
+  settings: {
+    selectedDuration: number;
+  };
+  statistics: {
+    sessions: any[];
+  };
+};
+
+const initialState = {
+  settings: {
+    selectedDuration: 300,
+  },
+  statistics: {
+    sessions: [],
+  },
+};
+
+const ACTIONS = {
+  SELECT_DURATION: 'select-duration',
+};
+
+const reducer = (state: AppState, action): AppState => {
+  /*   switch (action.type) {
+    case ACTIONS.SELECT_DURATION:
+      return {
+        ...state,
+        settings.selectedDuration = action.payload
+      }
+  } */
+};
 
 export const Root = () => {
-  const [appState, dispatch] = useReducer(reducer, {
-    settings: {
-      selectedDuration: '00:05:00',
-    },
-    statistics: {
-      sessions: [],
-    },
-  });
+  const [appState, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className="h-full min-h-screen w-screen bg-gradient-to-b from-green-100 to-blue-200 px-10 py-4">
