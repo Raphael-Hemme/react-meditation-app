9599369ac99802c29aafee526e7ebd065a6bbeaa
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-06-28 12:13:15 +0200

    core: Provide the appState and dispatch function via a context since it can't be passed as props to the child components because they are rendered dynamically insides a React Router outlet - probably not ideal because it is triggering rerenders throughout the page now, but the state should not become so complex or so frequently changing that it should be a problem

--- src/routes/SettingsPage.tsx
+++ src/routes/SettingsPage.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
 import { PageTitle } from '../components/PageTitle';
+import { ACTIONS, useAppState } from './Root';
 
 export const SettingsPage = () => {
   // Time in full minutes since sub-minute adjustment should not be necessary-> default is 5 minutes
   const [minuteDuration, setMinuteDuration] = useState(5);
+  const appState = useAppState();
 
   const handleDurationChange = (val: number) => {
     setMinuteDuration(val);
     // Todo: dispatch action to set app state
+    appState.dispatch({
+      type: ACTIONS.SELECT_DURATION,
+      payload: { selectedDuration: minuteDuration * 60 },
+    });
   };
 
   return (
@@ -31,6 +37,7 @@ export const SettingsPage = () => {
         </div>
         <p>How long do you want to meditate?</p>
       </section>
+      <h2>This should work: {appState.state.settings.selectedDuration}</h2>
     </div>
   );
 };
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { Menu } from '../components/Menu';
-import { useReducer } from 'react';
+import React, { useReducer, createContext } from 'react';
 
 type AppState = {
   settings: {
@@ -20,29 +20,55 @@ const initialState = {
   },
 };
 
-const ACTIONS = {
+export const ACTIONS = {
   SELECT_DURATION: 'select-duration',
 };
 
-const reducer = (state: AppState, action): AppState => {
-  /*   switch (action.type) {
+const reducer = (
+  state: AppState,
+  action: { type: string; payload: any }
+): AppState => {
+  switch (action.type) {
     case ACTIONS.SELECT_DURATION:
       return {
         ...state,
-        settings.selectedDuration = action.payload
-      }
-  } */
+        settings: {
+          ...state.settings,
+          selectedDuration: action.payload.selectedDuration,
+        },
+      };
+    default:
+      return state;
+  }
 };
+
+const AppStateContext = createContext<
+  | {
+      state: AppState;
+      dispatch: React.Dispatch<any>;
+    }
+  | undefined
+>(undefined);
 
 export const Root = () => {
   const [appState, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <div className="h-full min-h-screen w-screen bg-gradient-to-b from-green-100 to-blue-200 px-10 py-4">
-      <main className="h-full min-h-full w-full">
-        <Outlet />
-      </main>
-      <Menu />
-    </div>
+    <AppStateContext.Provider value={{ state: appState, dispatch }}>
+      <div className="h-full min-h-screen w-screen bg-gradient-to-b from-green-100 to-blue-200 px-10 py-4">
+        <main className="h-full min-h-full w-full">
+          <Outlet />
+        </main>
+        <Menu />
+      </div>
+    </AppStateContext.Provider>
   );
+};
+
+export const useAppState = () => {
+  const context = React.useContext(AppStateContext);
+  if (context === undefined) {
+    throw new Error('useAppState must be used within an AppStateProvider');
+  }
+  return context;
 };
